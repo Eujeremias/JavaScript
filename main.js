afd4db469a0a4b3d3a26a8e18ecedcab18dd9235
Jeremias Verissimo <jeremiasverissimo19@gmail.com>
2024-01-02 08:45:55 -0300

    .s

--- main.js
+++ main.js
@@ -1,60 +1,101 @@
-//Variáveis
+// //Variáveis
 
-// Loops
+// // Loops
 
-// Arrays e Objects
+// // Arrays e Objects
 
-let listaNumérica = [1, 2, 3, 4, 5];
-console.log(listaNumérica);
-console.log(typeof listaNumérica); // mostrará o tipo
-//Imprimir o array completo na tela
+// let listaNumérica = [1, 2, 3, 4, 5];
+// console.log(listaNumérica);
+// console.log(typeof listaNumérica); // mostrará o tipo
+// //Imprimir o array completo na tela
 
-// Criando objetos
-let pessoa = {
-  name: "Jeremias",
-  sobreNome: "Verissimo",
-  sexo: "Masculino",
-  idade: 20,
-};
+// // Criando objetos
+// let pessoa = {
+//   name: "Jeremias",
+//   sobreNome: "Verissimo",
+//   sexo: "Masculino",
+//   idade: 20,
+// };
 
-// Adicionando uma chave ao nosso objeto "pessoa"
-pessoa.altura = 1.68;
+// // Adicionando uma chave ao nosso objeto "pessoa"
+// pessoa.altura = 1.68;
 
-let pessoa2 = {
-  name: "Amanda",
-  sobreNome: "Silva",
-  sexo: "Feminino",
-  idade: 22,
-};
+// let pessoa2 = {
+//   name: "Amanda",
+//   sobreNome: "Silva",
+//   sexo: "Feminino",
+//   idade: 22,
+// };
 
-// pondo em uma variável a união dos objetos.
-let pessoas = Object.assign(pessoa, pessoa2);
-console.log(pessoas);
+// // pondo em uma variável a união dos objetos.
+// let pessoas = Object.assign(pessoa, pessoa2);
+// console.log(pessoas);
 
-// O object é um método que através dele conseguimos acessar outros métodos para fazer algo.
-// Na linha 27 unimos dois objetos com o método Object.assign
+// // O object é um método que através dele conseguimos acessar outros métodos para fazer algo.
+// // Na linha 27 unimos dois objetos com o método Object.assign
 
-//Abaixo há outros métodos. Faça o teste
-console.log(Object.keys(pessoa));
-console.log(Object.values(pessoa));
+// //Abaixo há outros métodos. Faça o teste
+// console.log(Object.keys(pessoa));
+// console.log(Object.values(pessoa));
 
-// Mutação 🧟
+// // Mutação 🧟
 
-let listObject = [
-  {
-    name: "jeremias",
-    lastName: "Verissimo",
-    years: 20,
-  },
-  {
-    name: "Maria",
-    lastName: "Silva",
-    years: 30,
-  },
-];
+// let listObject = [
+//   {
+//     name: "jeremias",
+//     lastName: "Verissimo",
+//     years: 20,
+//   },
+//   {
+//     name: "Maria",
+//     lastName: "Silva",
+//     years: 30,
+//   },
+// ];
 
-let copy = listObject;
+// let copy = listObject;
 
-delete listObject[0].years;
+// delete listObject[0].years;
 
-console.log(listObject);
+// console.log(listObject);
+
+// // Alguns métodos do JavaScript
+// // ======POP | PUSH============
+// let list = [1, 2, 3];
+// list.push(4); //adicionar 4 no fim da lista
+// list.pop(); //remove o útimo item (4)
+
+// let removidos = list.pop();
+// console.log(removidos);
+
+// console.log(list);
+
+// //SHIFT | UNSHIFT
+
+// let letters = ["a", "b", "c"];
+// letters.unshift("d");
+// letters.shift(); // remove o primeiro da lista (d)
+// console.log(letters);
+
+// Slice -
+let testeSlice = ["a", "b", "c", "d", "e", "f"];
+const subArray = testeSlice.slice(0, 7);
+console.log(subArray);
+
+// padStart | padEnd | split
+
+//split transoforma uma string em array, tendo como parâmetro o que as separa como frase
+const frase = "Fui a casa do meu amigo e comi batata frita";
+console.log(frase.split(" ")); //string de espaçamento que indica espaço entre as palavras
+
+// padStart e padEnd acrescenta caracteres antes e no fim de uma string
+
+const name = "jeremias";
+const name1 = name.padStart(10, "@");
+
+console.log(name1.padEnd(11, "@"));
+
+//Split faz o contrário do split, transforma um array em string
+// const names = ["Jeremy", "John", "Jackson", "James"];
+const itens = ["Mouse", "Keyboard", "Monitor", "Desktop"];
+console.log(`Itens a serem comprados: ${itens.join(", ")}`);
--- readme.md
+++ readme.md
@@ -19,7 +19,7 @@ Aqui será tratados alguns tópicos importantes da programação com JavaScript.
 
 * E podemos ter sim uma lista de objetos
 
-    <pre>
+  <pre>
     <code>
       let person =[
         {
@@ -53,4 +53,57 @@ Aqui será tratados alguns tópicos importantes da programação com JavaScript.
     console.log(list)//imprimirá [1, undefined]
   </code></pre>
 
-## Mais método de JavaScript ![1704028749828](image/readme/1704028749828.png)
+## Mais método de JavaScript
+
+### Pop e Push
+
+    1. Push adiciona um item ao final da lista
+    2. Pop retira um item ao final da lista
+
+   <pre><code>
+    let list = [1,2,3]
+    list.push(4)//adicionar 4 no fim da lista
+    list.pop //remove o item adicionado (4)
+   </code></pre>
+
+O legal é que podemos ver o úlitmo item removido pelo pop. Veja o exemplo:
+
+<pre><code>console.log(list.pop())</code></pre>
+
+## Shif e Unshift
+
+1. Shift remove o primeiro item da lista
+2. Unshift adiciona um item primeiro na lista.
+   <pre><code>
+    let letters = ["a", "b", "c"];
+    letters.unshift("d");
+    letters.shift(); // remove o primeiro da lista (d)
+    console.log(letters);
+   </code></pre>
+
+## Slice
+
+let lista = ["Jeremias", "Henrique", "Leandro", "Luiza"];
+// console.log(lista.slice(1)); //retorna os elementos a partir do indice 1 até o final
+// console.log(lista.slice(-2)); // retorna dos últimos 2 itens
+console.log(lista.slice(1, 2)); // primeiro valor será onde começará a contagem. Já o segundo o último.
+
+<pre><code>
+  // padStart | padEnd | split
+
+//split transoforma uma string em array, tendo como parâmetro o que as separa como frase
+const frase = "Fui a casa do meu amigo e comi batata frita";
+console.log(frase.split(" ")); //string de espaçamento que indica espaço entre as palavras
+
+// padStart e padEnd acrescenta caracteres antes e no fim de uma string
+
+const name = "jeremias";
+const name1 = name.padStart(10, "@");
+
+console.log(name1.padEnd(11, "@"));
+
+//Split faz o contrário do split, transforma um array em string
+// const names = ["Jeremy", "John", "Jackson", "James"];
+const itens = ["Mouse", "Keyboard", "Monitor", "Desktop"];
+console.log(`Itens a serem comprados: ${itens.join(", ")}`);
+</code></pre>
